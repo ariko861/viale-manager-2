955204476414e2021f5cbde46afb7b49e17f7d6d
Arthur Briquet <mdpbriar@henallux.be>
2024-05-25 14:56:15 +0200

    feat: ajout policy profile

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -4,12 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Profile extends Model
 {
     use HasFactory;
 
     protected $fillable = ["name", "price", "remarques", "is_default"];
+
+    public function sejours(): HasMany
+    {
+        return $this->hasMany(Sejour::class);
+    }
 
     public function getEuroAttribute()
     {
